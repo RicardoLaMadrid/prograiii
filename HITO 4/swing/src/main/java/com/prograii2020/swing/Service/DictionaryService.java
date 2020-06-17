df7c419d736df2df62489fb2d959e8c55932f0d8
Ricardo La Madrid <ricardolm160@gmail.com>
2020-06-17 11:34:37 -0400

    Hito4

--- HITO 4/swing/src/main/java/com/prograii2020/swing/Service/DictionaryService.java
+++ HITO 4/swing/src/main/java/com/prograii2020/swing/Service/DictionaryService.java
@@ -12,27 +12,27 @@ import java.util.List;
 
 @Service
 public class DictionaryService implements DictionaryServiceInterface{
-//    private static final String Monday = "Monday";
-//    private static final String Tuesday = "Tuesday";
-//    private static final String Wednesday = "Wednesday";
-//    private static final String Thursday = "Thursday";
-//    private static final String Friday = "Friday";
-//    private static final String Saturday = "Saturday";
-//    private static final String Sunday = "Sunday";
-//    private static final String SegundaFeira = "Segunda_Feira";
-//    private static final String Terca = "Terca-Feira";
-//    private static final String Quarta = "Quarta-Feira";
-//    private static final String Quinta = "Quinta-Feira";
-//    private static final String Sexta = "Sexta-Feira";
-//    private static final String SabadoP = "Sabado";
-//    private static final String DomingoP = "Domingo";
-//    private static final String Lunes = "Lunes";
-//    private static final String Martes = "Martes";
-//    private static final String Miercoles = "Miercoles";
-//    private static final String Jueves = "Jueves";
-//    private static final String Viernes = "Viernes";
-//    private static final String Sabado = "Sabado";
-//    private static final String Domingo = "Domingo";
+    private static final String Monday = "Monday";
+    private static final String Tuesday = "Tuesday";
+    private static final String Wednesday = "Wednesday";
+    private static final String Thursday = "Thursday";
+    private static final String Friday = "Friday";
+    private static final String Saturday = "Saturday";
+    private static final String Sunday = "Sunday";
+    private static final String SegundaFeira = "Segunda_Feira";
+    private static final String Terca = "Terca-Feira";
+    private static final String Quarta = "Quarta-Feira";
+    private static final String Quinta = "Quinta-Feira";
+    private static final String Sexta = "Sexta-Feira";
+    private static final String SabadoP = "Sabado";
+    private static final String DomingoP = "Domingo";
+    private static final String Lunes = "Lunes";
+    private static final String Martes = "Martes";
+    private static final String Miercoles = "Miercoles";
+    private static final String Jueves = "Jueves";
+    private static final String Viernes = "Viernes";
+    private static final String Sabado = "Sabado";
+    private static final String Domingo = "Domingo";
 
 
     @Autowired
@@ -41,22 +41,14 @@ public class DictionaryService implements DictionaryServiceInterface{
     @Override
     public void saveData() {
         if (dictionaryRepository.count() == 0) {
-//            dictionaryRepository.save(new DictionaryModel(Monday,SegundaFeira,Lunes));
-//            dictionaryRepository.save(new DictionaryModel(Tuesday,Terca,Martes));
-//            dictionaryRepository.save(new DictionaryModel(Wednesday,Quarta,Miercoles));
-//            dictionaryRepository.save(new DictionaryModel(Thursday,Quinta, Jueves));
-//            dictionaryRepository.save(new DictionaryModel(Friday,Sexta,Viernes));
-//            dictionaryRepository.save(new DictionaryModel(Saturday,SabadoP,Sabado));
-//            dictionaryRepository.save(new DictionaryModel(Sunday,DomingoP,Domingo));
+            dictionaryRepository.save(new DictionaryModel(Monday,SegundaFeira,Lunes));
+            dictionaryRepository.save(new DictionaryModel(Tuesday,Terca,Martes));
+            dictionaryRepository.save(new DictionaryModel(Wednesday,Quarta,Miercoles));
+            dictionaryRepository.save(new DictionaryModel(Thursday,Quinta, Jueves));
+            dictionaryRepository.save(new DictionaryModel(Friday,Sexta,Viernes));
+            dictionaryRepository.save(new DictionaryModel(Saturday,SabadoP,Sabado));
+            dictionaryRepository.save(new DictionaryModel(Sunday,DomingoP,Domingo));
 
-
-            dictionaryRepository.save(new DictionaryModel("Monday","Segunda-Feira","Lunes"));
-            dictionaryRepository.save(new DictionaryModel("Tuesday","Terca-Feira","Martes"));
-            dictionaryRepository.save(new DictionaryModel("Wednesday","Quarta-Feira","Miercoles"));
-            dictionaryRepository.save(new DictionaryModel("Thursday","Quinta-Feira","Jueves"));
-            dictionaryRepository.save(new DictionaryModel("Friday","Sexta-Feira","Viernes"));
-            dictionaryRepository.save(new DictionaryModel("Saturday","Sabado","Sabado"));
-            dictionaryRepository.save(new DictionaryModel("Sunday","Domingo","Domingo"));
 
         }
     }
